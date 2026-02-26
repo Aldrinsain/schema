17399d030bb28b0f550c8759227b2c628fc6300e
Aldrinsain <angelasain86@gmail.com>
2026-02-25 23:52:48 -0800

    added schema.sql

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE chicken_grazing_monitoring;
+USE chicken_grazing_monitoring;
+
+-- Users Table
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    date_info DATE NOT NULL       -- storing a date instead of password
+);
+
+-- Chickens Table
+CREATE TABLE chickens (
+    chicken_id INT AUTO_INCREMENT PRIMARY KEY,
+    breed VARCHAR(50)
+);
+
+-- User Logs Table
+CREATE TABLE user_logs (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    chicken_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (chicken_id) REFERENCES chickens(chicken_id)
+);
